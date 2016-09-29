756b1972e24f8e30dc1c2d507ae1fda9a7af365d
Grace Andrews <grace.e.andrews@gmail.com>
2016-09-29 14:33:31 -0700

    update users.pp

--- users.pp
+++ users.pp
@@ -2,7 +2,7 @@ class profile::windows::users {
 
   dsc_xaduser { 'ADuser':
     dsc_ensure       => 'present',
-    dsc_username    => 'Athena Andrews',
+    dsc_username     => 'Athena Andrews',
     dsc_domainname   => 'puppet.local',
     dsc_emailaddress => 'athena@puppet.com',
     dsc_path         => 'OU=DemoUsers,DC=puppet',
