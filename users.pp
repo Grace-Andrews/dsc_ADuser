96a83d58647829600f9985eb8d00948d08fbcc66
root <root@master.inf.puppet.vm>
2016-09-29 21:31:02 +0000

    add AD users profile

---
+++ users.pp
@@ -0,0 +1,11 @@
+class profile::windows::users {
+
+  dsc_xaduser { 'ADuser':
+    dsc_ensure       => 'present',
+    dsc_username    => 'Athena Andrews',
+    dsc_domainname   => 'puppet.local',
+    dsc_emailaddress => 'athena@puppet.com',
+    dsc_path         => 'OU=DemoUsers,DC=puppet',
+  }
+
+}
